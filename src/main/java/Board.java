7b36673fb04b74676d00de39f092420c4fe4b1c9
MatLeg25 <mat.legierskii@gmail.com>
2021-03-04 22:40:14 +0100

    Code order. CheckMove updated

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,15 +1,34 @@
 public class Board {
 
-    String Player1symbol = "\033[0;31m O \033[0m"; //red
-    String Player2symbol = "\033[0;33m O \033[0m"; // yellow
+    static String Player1symbol;// = "\033[0;31m O \033[0m"; //red
+    static String Player2symbol;// = "\033[0;33m O \033[0m"; // yellow
+    static String emptyField;
 
-    static int size = 10;
-    static String[] labelX = new String[size];
-    static String[] labelY = new String[size];
-    public static Pawn[][] fields = new Pawn[2 * size][2 * size];
+    //create variable of this class
+    static int size;
+    static int rw;
+    static String[] labelX;
+    static String[] labelY;
+    public static Pawn[][] fields;
 
-    public Board(int size, int rw) {
+    //set the values for variable of this class
+    public static void SetInitData(int Bsize, String [] symbols) {
+        Board.size =Bsize;
 
+        if (Bsize%2!=0) {Bsize+=1;}
+        Board.rw = (Bsize/2)-1; //define number of rows with pawns
+
+        labelX = new String[size];
+        labelY = new String[size];
+        fields = new Pawn[2 * size][2 * size];
+
+        Player1symbol = symbols[0];
+        Player2symbol = symbols[1];
+        emptyField = symbols[2];
+    }
+
+    public Board() {
+        //create 1object BOARD  and objects Pawns with appropriate positions
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 if ((y % 2 == 1 && x % 2 == 0 && x < rw) || (y % 2 == 0 && x % 2 == 1 && x < rw)) {
@@ -24,10 +43,9 @@ public class Board {
                 }
             }
         }
-
     }
 
-    public static void displayBoard(int size) {
+    public static void displayBoard() {
 
         //fill label X
         for (int i = 0; i < size; i++) {
@@ -47,7 +65,7 @@ public class Board {
                 if (fields[x][y] != null) { //field = object
                     System.out.print(fields[x][y].playerSymbol+" | ");
                 } else {
-                    System.out.print("\033[0;30m . \033[0m | "); //empty field = null
+                    System.out.print(emptyField); //empty field = null
                 }
             }System.out.println();
         }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,54 +1,78 @@
 import java.util.Scanner;
-import java.io.IOException;
 
 public class Game {
-    public static void main(String args[]) throws IOException, InterruptedException {
+
+    private static String Player1symbol = "\033[0;31m O \033[0m"; //red
+    private static String Player2symbol = "\033[0;33m O \033[0m"; // yellow
+    private static String emptyField = "\033[0;30m . \033[0m | ";
+    private static String[] symbols = {Player1symbol,Player2symbol,emptyField};
+
+    private static int gameBoardSize;
+
+    public static void main(String args[]) {
         boolean game = true;
         int counter=0;
-
+        String currentPlayerSymbol;
         String Player;
-        String Player1symbol = "\033[0;31m O \033[0m"; //red
-        String Player2symbol = "\033[0;33m O \033[0m"; // yellow
-
-        System.out.println("COUNTER= "+counter);
-        String playerSymbol;
+        int[] PawnCount;
 
         start();
-
         System.out.println();
 
         while (game) {
 
-            if (counter%2==0) {playerSymbol = Player1symbol;Player="Player1"; } else {playerSymbol = Player2symbol;Player="Player2";}
-            System.out.println("PLAYER: "+Player);
+            PawnCount = countPawns(gameBoardSize);
+            System.out.println("\nPAWN COUNT: Player1="+PawnCount[0]+" ,Player2="+PawnCount[1]);
 
-            int[] changePositions = round(Player);
+            if (counter%2==0) {currentPlayerSymbol = Player1symbol;Player="Player1"; } else {currentPlayerSymbol = Player2symbol;Player="Player2";}
+            System.out.println("\nCURRENT PLAYER: "+Player);
 
-            if (checkMove(changePositions)) {
-                setPosition(changePositions, Player);
-                pawnElminated(changePositions, playerSymbol);
-            } else {
-                System.out.println("Wrong move, try again! \n");
+            Board.displayBoard();
+
+            int[] getOldPosition = getPosition("Select token to move: ");
+            int oldX = getOldPosition[0];
+            int oldY = getOldPosition[1];
+
+            if (!checkSelectedPawn(oldX,oldY, currentPlayerSymbol)) {
                 continue;
             }
-            if (checkWinners(countPawns(10))) {
+
+            highlightPawn(oldX, oldY, Player);
+            Board.displayBoard();
+
+            int[] getNewPosition = getPosition("Define new position: ");
+            int newX = getNewPosition[0];
+            int newY = getNewPosition[1];
+
+            int[] positions = {oldX, oldY, newX, newY};
+
+
+            int[] changePositions = positions; //round(Player);
+
+            if (checkMove(changePositions, currentPlayerSymbol)) {
+                setPosition(changePositions, currentPlayerSymbol);
+                pawnElminated(changePositions, currentPlayerSymbol);
+                clearHighlightPawn(newX,newY,Player);
+            } else {
+                System.out.println("Wrong move, try again! \n");
+                clearHighlightPawn(oldX,oldY,Player);
+                continue;
+            }
+            if (checkWinners(countPawns(Game.gameBoardSize))) {
                 game = false;
                 };
             //System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-            Board.displayBoard(10);
             counter++;
         }
     }
 
     public static void start() {
-        /*        System.out.println("Give a board size: ");
+        System.out.println("\nGive a board size: ");
         Scanner boardSize = new Scanner(System.in);
         int n = boardSize.nextInt(); //n between 4-20;
-        */
-        int n = 10;
-        int rw = 4;
-        new Board(n, rw);
-        Board.displayBoard(n);
+        Game.gameBoardSize = n;
+        Board.SetInitData(n, symbols);
+        new Board();
     }
 
     public static int[] round(String Player) {
@@ -57,9 +81,9 @@ public class Game {
         int oldX = oldPosition[0];
         int oldY = oldPosition[1];
 
-        higlightPawn(oldX,oldY,Player);
+        highlightPawn(oldX, oldY, Player);
 
-        Board.displayBoard(10);
+        Board.displayBoard();
 
         int[] newPosition = getPosition("Define new position: ");
         int newX = newPosition[0];
@@ -71,12 +95,11 @@ public class Game {
 
     public static int[] getPosition(String info) {
         System.out.println(info);
+        Scanner userPosition = new Scanner(System.in);
         System.out.print("Type row(X): ");
-        Scanner userPositionX = new Scanner(System.in);
-        int newPosX = userPositionX.nextInt();
+        int newPosX = userPosition.nextInt();
         System.out.print("Type column(Y): ");
-        Scanner newUserPositionY = new Scanner(System.in);
-        int newPosY = userPositionX.nextInt();
+        int newPosY = userPosition.nextInt();
 
         int[] temp = {newPosX, newPosY};
 
@@ -90,10 +113,9 @@ public class Game {
         int newY = changePos[3];
         Board.fields[newX][newY] = Board.fields[oldX][oldY];
         Board.fields[oldX][oldY] = null;
-        clearHiglightPawn(newX,newY,Player);
     }
 
-    public static boolean checkMove(int[] changePos) {
+    public static boolean checkMove(int[] changePos, String GamerSymbol) {
         int oldX = changePos[0];
         int oldY = changePos[1];
         int newX = changePos[2];
@@ -102,72 +124,93 @@ public class Game {
         int oPpositionX = (oldX+newX)/2;
         int oPpositionY = (oldY+newY)/2;
 
-        if (Board.fields[oldX][oldY] == null) {
-            System.out.println("\nGrab correct pawn!");
+        String SelectedPawnSymbol = Board.fields[oldX][oldY].playerSymbol;
+
+        //check if given position are coordinate of current players pawn
+        if (checkSelectedPawn(oldX,oldY,SelectedPawnSymbol)) {
+        } else {
             return false;
         }
 
         if ((Math.abs(newX-oldX) > 2 ) ||  (Math.abs(newY-oldY) > 2)) {
+            System.out.print("\nYou can not move there!\n");
             return false;
         }
 
         if ((Math.abs(newX-oldX) == 2 ) ||  (Math.abs(newY-oldY) == 2)) {
             if (Board.fields[oPpositionX][oPpositionY] == null) {
+                System.out.print("\nYou can not move about 2field if you do not attack enemy!\n");
+                return false;
+            } else if (Board.fields[oPpositionX][oPpositionY].playerSymbol == GamerSymbol) {
+                System.out.print("\nYou can not move above your pawn!\n");
                 return false;
             }
             return true;
         }
 
-        String Player = Board.fields[oldX][oldY].playerSymbol;
-
         if (Board.fields[newX][newY] == null) {
-            return true;
+            //check that the movement is 1 point on diagonal
+            if ((Math.abs(newX-oldX) == 1 ) &&  (Math.abs(newY-oldY) == 1)) {
+                return true;
+            } else {
+                System.out.println("\nYou can move only in the diagonals direction");
+                return false;}
         } else {
             return false;
         }
     }
 
+    public static boolean checkSelectedPawn(int x, int y, String GamerSymbol) {
+        if (Board.fields[x][y] == null) {
+            System.out.println("\nIncorrect coordinates! Grab correct pawn!");
+            return false;
+        }
+        if (Board.fields[x][y].playerSymbol != GamerSymbol) {
+            System.out.println("Select your pawn!");
+            return false;
+        }
+        return true;
+    }
+
     public static int[] countPawns(int size) {
-        String Player1symbol = "\033[0;31m O \033[0m"; //red
-        String Player2symbol = "\033[0;33m O \033[0m"; // yellow
-        int cW = 0;
-        int cB = 0;
+        int cP1 = 0;
+        int cP2 = 0;
         for (int x = 0; x < size; x++) {
             for (int y = 0; y < size; y++) {
                 if (Board.fields[x][y] != null) {
-                    if (Board.fields[x][y].playerSymbol == Player1symbol) {
-                        cW++;
-                    } else if (Board.fields[x][y].playerSymbol == Player2symbol) {
-                        cB++;
+                    if (Board.fields[x][y].playerSymbol == Game.Player1symbol) {
+                        cP1++;
+                    } else if (Board.fields[x][y].playerSymbol == Game.Player2symbol) {
+                        cP2++;
                     }
                 }
             }
         }
-        int[] results = {cW, cB};
+        int[] results = {cP1, cP2};
         return results;
     }
 
     public static boolean checkWinners(int[] data) {
-        int cW = data[0];
-        int cB = data[1];
+        int cP1 = data[0];
+        int cP2 = data[1];
 
-        if (cW == 0) {
-            System.out.println("Game over. The winner is RED");
+        if (cP2 == 0) {
+            System.out.println("\n\nGame over. The winner is Player1");
             return true;
         }
-        if (cB == 0) {
-            System.out.println("Game over. The winner is YELLOW");
+        if (cP1 == 0) {
+            System.out.println("\n\nGame over. The winner is Player2");
             return true;
         }
         return false;
     }
 
-    public static boolean pawnElminated(int[] changePos, String opponent) {
+    public static boolean pawnElminated(int[] changePos, String gamerSymbol) {
         int oldX = changePos[0];
         int oldY = changePos[1];
         int newX = changePos[2];
         int newY = changePos[3];
-
+        //System.out.println(oldX+"|"+oldY+"|"+newX+"|"+newY);
         int oPpositionX = (oldX+newX)/2;
         int oPpositionY = (oldY+newY)/2;
 
@@ -175,7 +218,7 @@ public class Game {
         double xCheck = Math.abs(newX-oldX);
         if (xCheck == 2) {
             System.out.println("ELIMINATED: "+oPpositionX + "|" + oPpositionY);
-            if (Board.fields[oPpositionX][oPpositionY].playerSymbol == opponent) {
+            if (Board.fields[oPpositionX][oPpositionY].playerSymbol != gamerSymbol) {
                 Board.fields[oPpositionX][oPpositionY] = null;
                 return true;
             }
@@ -184,20 +227,21 @@ public class Game {
         return false;
     }
 
-    public static void higlightPawn(int X, int Y, String Gamer) {
+    public static void highlightPawn(int X, int Y, String Gamer) {
+
         if (Gamer=="Player1") {
-            Board.fields[X][Y].playerSymbol = "\033[1;92m O \033[0m"; //green
-        } else {
             Board.fields[X][Y].playerSymbol = "\033[0;35m O \033[0m";
+        } else {
+            Board.fields[X][Y].playerSymbol = "\033[1;92m O \033[0m"; //green
         }
     }
 
-    public static void clearHiglightPawn(int X, int Y,String Gamer) {
-        System.out.println(X+" |"+Y+"|"+Gamer);
+    public static void clearHighlightPawn(int X, int Y,String Gamer) {
+        //System.out.println(X+" |"+Y+"|"+Gamer);
         if (Gamer=="Player1") {
-            Board.fields[X][Y].playerSymbol = "\033[0;33m O \033[0m";
+            Board.fields[X][Y].playerSymbol = Game.Player1symbol; //"\033[0;31m O \033[0m";
         } else {
-            Board.fields[X][Y].playerSymbol = "\033[0;31m O \033[0m";
+            Board.fields[X][Y].playerSymbol = Game.Player2symbol; //"\033[0;33m O \033[0m";
         }
     }
 }
